6e786c77327f610244a7c72af04305f63ecd86cd
cmgrier <cmgrier@wpi.edu>
2017-11-13 21:32:03 -0500

    Problem 3

--- HW3/src/Earthquake1.java
+++ HW3/src/Earthquake1.java
@@ -8,9 +8,33 @@ class Earthquake1 {
   // extracts the month from an 8-digit date
   int extractMonth(double dateNum) { return ((int)dateNum % 10000) / 100; }
  
-  public LinkedList<MaxHzReport> dailyMaxForMonth(LinkedList<Double> data,
-                                                  int month) {
-   return null;
+  public LinkedList<MaxHzReport> dailyMaxForMonth(LinkedList<Double> data, int month) {
+	  LinkedList<MaxHzReport> resultList = new LinkedList<MaxHzReport>();
+	  double max = 0;
+	  double date = 0;
+
+	  for(int i = 0; i < data.size(); i++) {
+		  if(isDate(data.get(i))) {
+			  if(extractMonth(data.get(i)) == month) {
+				  if(date == 0) {
+					  date = data.get(i);
+				  }
+				  else if(date > 0) {
+					  resultList.add(new MaxHzReport(date, max));
+					  date = data.get(i);
+					  max = 0;
+				  }
+			  }
+		  }
+		  else {
+			  if (date != 0) {
+				  if(max < data.get(i)) {
+					  max = data.get(i);			  
+			  }
+		     }
+		    }
+		   }
+	  resultList.add(new MaxHzReport(date, max));
+	  return resultList;
   }
 }  
-
