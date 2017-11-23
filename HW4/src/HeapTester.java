c812f90df25e6137caa71c71c9c3467a5f6b2e80
cmgrier <cmgrier@wpi.edu>
2017-11-22 22:01:18 -0500

    Setup of HW 4
    Not sure if it is setup correctly.  Hopefully it is

---
+++ HW4/src/HeapTester.java
@@ -0,0 +1,13 @@
+
+public class HeapTester {
+	  boolean addEltTester(IHeap hOrig, int elt, IBinTree hAdded) {
+
+		    //...code to compare hOrig and hAdded around the addition of elt as appropriate...
+		  return true;
+		  }
+	  boolean remMinEltTester(IHeap hOrig, IBinTree hRemoved) {
+
+		    //...code to compare hOrig and hRemoved as appropriate...
+		  return true;
+		  }
+}
