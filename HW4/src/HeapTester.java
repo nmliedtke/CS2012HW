621c4b13743daf60301e9cb9290c5729fce26c01
cmgrier <cmgrier@wpi.edu>
2017-11-26 19:24:55 -0500

    AddHelpTester

--- HW4/src/HeapTester.java
+++ HW4/src/HeapTester.java
@@ -1,10 +1,22 @@
 
 public class HeapTester {
 	  boolean addEltTester(IHeap hOrig, int elt, IBinTree hAdded) {
-
 		    //...code to compare hOrig and hAdded around the addition of elt as appropriate...
-		  return true;
+		  boolean one = hAdded.hasElt(elt);
+		  
+		  boolean two = hAdded.isHeap();
+		  
+		  boolean three = hAdded.contains(hOrig);
+		  
+		  boolean four = ((hOrig.size() + 1) == hAdded.size());
+		  
+		  return (one && two && three && four);
 		  }
+	  // hasElt(elt, hAdded) check if elt is in the hAdded
+	  // is hAdded a heap
+	  // are all of the other elt in hOrig in the hAdded
+	  // is the size correct
+	  
 	  boolean remMinEltTester(IHeap hOrig, IBinTree hRemoved) {
 
 		    //...code to compare hOrig and hRemoved as appropriate...
