b652839e1e11c880e2deb494c5778092172d0ffb
cmgrier <cmgrier@wpi.edu>
2017-11-27 22:11:08 -0500

    update

--- HW4/src/Examples.java
+++ HW4/src/Examples.java
@@ -1,4 +1,14 @@
+import org.junit.jupiter.api.Test;
 
 public class Examples {
+	 Examples(){}
 
+	    HeapTester HT = new HeapTester();
+
+	    
+
+	    @Test
+	    public void test3(){
+	        assertTrue(HT.addEltTester(myHeap, 5, myBinTree));
+	    }   
 }
