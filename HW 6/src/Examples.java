135990c1be02d7a10b7b7e09a5f9489134ec7d6e
cmgrier <cmgrier@wpi.edu>
2017-12-08 19:49:26 -0500

    HW 6

---
+++ HW 6/src/Examples.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class Examples {
+	Examples(){
+		System.out.println("The candidates are ");
+
+	}
+	@Test
+	public void check() {
+
+		assertEquals(1, 1+0);
+	}
+	
+}
