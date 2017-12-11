6fbb0b9c3ab4968c91c3e079935f733245581bec
nmliedtke <32607974+nmliedtke@users.noreply.github.com>
2017-12-10 23:28:25 -0500

    vote class

---
+++ HW 6/src/Vote.java
@@ -0,0 +1,14 @@
+
+public class Vote {
+	
+	String firstChoice;
+	String secondChoice;
+	String thirdChoice;
+	
+	Vote(String firstChoice, String secondChoice, String thirdChoice){
+		this.firstChoice = firstChoice;
+		this.secondChoice = secondChoice;
+		this.thirdChoice = thirdChoice;
+	}
+
+}
