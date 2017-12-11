ca7b956be0d4c9c0a7d2ae0bf14a6f40d607d0ca
nmliedtke <32607974+nmliedtke@users.noreply.github.com>
2017-12-10 23:29:13 -0500

    vote class again

--- HW 6/src/Vote.java
+++ HW 6/src/Vote.java
@@ -1,14 +1,26 @@
 
 public class Vote {
 	
-	String firstChoice;
-	String secondChoice;
-	String thirdChoice;
+	private String firstChoice;
+	private String secondChoice;
+	private String thirdChoice;
 	
 	Vote(String firstChoice, String secondChoice, String thirdChoice){
 		this.firstChoice = firstChoice;
 		this.secondChoice = secondChoice;
 		this.thirdChoice = thirdChoice;
+	}
+	
+	String getFirstChoice() {
+		return this.firstChoice;
+	}
+	
+	String getSecondChoice() {
+		return this.secondChoice;
+	}
+	
+	String getThirdChoice() {
+		return this.thirdChoice;
 	}
 
 }
